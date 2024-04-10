22cbc900533f71c677487c9e6046ffc43ea8f3e7
Nikhil Dubey <123150280+nikhildubey-23@users.noreply.github.com>
2024-04-10 07:33:20 +0530

    importing important module

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#import necessary module
+import socket
+import scapy.all as scapy
+import prettytable
+from mac_vendor_lookup import MacLookup
+from argparse import ArgumentParser
+import sys
+import struct
